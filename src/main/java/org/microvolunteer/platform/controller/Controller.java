a3a4953d43ee477a2d989906a4c1b84e30924ae7
urashin <s.urakawa@hotmail.co.jp>
2021-12-01 23:50:36 +0900

    api mock

--- src/main/java/org/microvolunteer/platform/controller/Controller.java
+++ src/main/java/org/microvolunteer/platform/controller/Controller.java
@@ -15,12 +15,9 @@ import org.springframework.web.bind.annotation.*;
 public class Controller {
     private Logger logger = LoggerFactory.getLogger(Controller.class);
 
-    /**
-     * ☆新規ユーザー登録（よゆうがあれば作る、なければDBに直書）
-     */
 
     /**
-     * ★LINEとサービスのアカウント情報紐付けのためのonetimeurl発行API
+     * Pythonで実装
      * ☆token発行（時間あればつくる、なければ固定のtokenでおこなう）
      * @param loginRequest
      * @return
@@ -34,14 +31,52 @@ public class Controller {
     }
 
     /**
+     * ここら辺はPythonでJohnさんにおまかせ？
+     * ☆新規ユーザー登録（よゆうがあれば作る、なければDBに直書）
+     * ontimeurlから遷移したユーザー登録画面からよばれる
+     * @param loginRequest
+     * @return
+     */
+    @PostMapping("/user/{onetimepath}/register")
+    @ResponseBody
+    public RegisterResponse register(@PathVariable String onetimepath, @RequestBody LoginRequest loginRequest){
+        logger.info("ログインAPI: {}", onetimepath);
+        // user_idを生成
+        // Usersテーブルにユーザーを追加：passwordと一緒にUsersテーブルへ記録
+        // SnsIdテーブルを更新：UPDATE SnsId SET user_id = xxx, sns_id = sss WHERE SnsId.onetimepath = onetimepath;
+        return RegisterResponse.builder().result("OK").build();
+    }
+
+    /**
+     * ここら辺はPythonでJohnさんにおまかせ？
      * ★LINEとサービスのアカウント情報紐付けのためのonetimeurl発行API
      */
     @GetMapping("/user/onetimeurl")
     @ResponseBody
     public OnetimeurlResponse onetimeurl(@RequestBody OnetimeurlRequest onetimeurlRequest){
-        logger.info("onetimeurl API: {}", onetimeurlRequest.getUserId());
-
+        logger.info("onetimeurl API: {}", onetimeurlRequest.getSnsId());
+        // LINE(Botの初回発言が -> このAPI: sns_idからonetime_pathを生成し、SnsIdテーブルに記録
+        //
+        // LINE -> ID連携するためのページ -> パラメータ：sns_id, ユーザー入力：email&password -> このAPI
+        // tokenからuser_idを取得
+        // sns_id, user_id, sns_typeをSnsIdテーブルへ登録
         return OnetimeurlResponse.builder().onetimeurl("http://tokenXXXXXXXXXXXXXXXXXXXX").build();
+    }
+
+    /**
+     * ★LINE Botコールバック
+     * よくわからなくなってきたので、Johnさんに相談するw
+     */
+    @GetMapping("/user/line_message")
+    @ResponseBody
+    public LineMessageResponse lineMessage(@RequestBody LineMessageRequest lineMessageRequest){
+        logger.info("onetimeurl API: {}", lineMessageRequest.getSnsId());
+        // LINE(Botの初回発言が -> このAPI: sns_idからonetime_pathを生成し、SnsIdテーブルに記録
+        //
+        // LINE -> ID連携するためのページ -> パラメータ：sns_id, ユーザー入力：email&password -> このAPI
+        // tokenからuser_idを取得
+        // sns_id, user_id, sns_typeをSnsIdテーブルへ登録
+        return LineMessageResponse.builder().message("http://tokenXXXXXXXXXXXXXXXXXXXX").build();
     }
 
     /**
@@ -51,7 +86,8 @@ public class Controller {
     @ResponseBody
     public CheckInResponse checkin(@RequestBody CheckInRequest checkInRequest){
         logger.info("CheckIn API: {}", checkInRequest.getUserId());
-
+        // tokenからuser_idを取得
+        // user_idと位置座標をMyGEOMETRYテーブルに登録
         return CheckInResponse.builder().result("OK").build();
     }
 
@@ -62,7 +98,8 @@ public class Controller {
     @ResponseBody
     public HelpResponse help(@RequestBody HelpRequest helpRequest){
         logger.info("CheckIn API: {}", helpRequest.getUserId());
-
+        // 対象ボランティアの抽出（マッチング）
+        // 対象ボランティアへのpush通知(python APIを使う)
         return HelpResponse.builder().result("OK").build();
     }
 
@@ -116,6 +153,8 @@ public class Controller {
     /**
      * ★line callbackで通知の仕組みを作る
      * ★自分の評価一覧
+     * ☆QRコード生成（引数のURLに入れておくもの：
+     * ☆QRコード解釈（url取得→URLに入れる情報
      * ☆自分の評価を公開するようのurl発行
      * ☆指定スポット付近でよく発生する困りごと（自分の障害でフィルター可能）
      */
--- src/main/java/org/microvolunteer/platform/resource/request/LineMessageRequest.java
+++ src/main/java/org/microvolunteer/platform/resource/request/LineMessageRequest.java
@@ -3,6 +3,7 @@ package org.microvolunteer.platform.resource.request;
 import lombok.Data;
 
 @Data
-public class OnetimeurlRequest {
+public class LineMessageRequest {
     private String snsId;
+    private String message;
 }
