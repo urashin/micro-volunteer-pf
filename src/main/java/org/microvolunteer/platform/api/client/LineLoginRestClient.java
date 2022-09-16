c66e455d6f834f1957e06fe8c8bbd869b923a7ba
urashin <s.urakawa@hotmail.co.jp>
2022-09-16 09:09:37 +0900

    refactor: コメントしたコードなどの削除

--- src/main/java/org/microvolunteer/platform/api/client/LineLoginRestClient.java
+++ src/main/java/org/microvolunteer/platform/api/client/LineLoginRestClient.java
@@ -1,7 +1,5 @@
 package org.microvolunteer.platform.api.client;
 
-import org.microvolunteer.platform.domain.resource.request.LineLoginRequest;
-import org.microvolunteer.platform.domain.resource.request.LineTokenRequest;
 import org.microvolunteer.platform.domain.resource.response.LineTokenResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -36,27 +34,11 @@ public class LineLoginRestClient {
     @Value("${line-login.login_redirect_uri}")
     private String login_redirect_uri;
 
-    //@Value("${line-login.oauth_redirect_uri}")
-    //private String oauth_redirect_uri;
-
     public String lineAuth(String code) {
         RestTemplate restTemplate = new RestTemplate();
 
-        //String line_token_url = "https://api.line.me/oauth2/v2.1/token";
-        //String redirect_uri = "http://127.0.0.1:8080/v1/api/auth";
-
-        /*
-        LineTokenRequest lineTokenRequest = LineTokenRequest.builder()
-                .grant_type("authorization_code")
-                .code(code)
-                .client_id(client_id)
-                .client_secret(client_secret)
-                .redirect_uri(login_redirect_uri)
-                .build();
-         */
 
         MultiValueMap<String,String> map = new LinkedMultiValueMap<>();
-        //map.add("grant_type","authorization_code");
         map.add("grant_type","authorization_code");
         map.add("code",code);
         map.add("client_id",client_id);
@@ -64,13 +46,10 @@ public class LineLoginRestClient {
         map.add("redirect_uri",login_redirect_uri);
 
         try {
-            //RequestEntity<LineTokenRequest> request = RequestEntity
             RequestEntity request = RequestEntity
                     .post(URI.create(line_token_url))
                     .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                    //.accept(MediaType.APPLICATION_FORM_URLENCODED)
                     .body(map);
-                    //.body(lineTokenRequest);
             ResponseEntity<LineTokenResponse> response = restTemplate.exchange(request, LineTokenResponse.class);
             String id_token = response.getBody().getId_token();
             return id_token;
